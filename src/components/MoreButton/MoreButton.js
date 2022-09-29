f4317f5b10523449b6cd344c2c69a3fbb06249dd
Melisa Çalışkan <melisaclskn5461@gmail.com>
2022-09-30 02:41:38 +0300

    add more button and style

---
+++ src/components/MoreButton/MoreButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MoreButton.css'
+function MoreButton() {
+  return (
+    <div>
+        <button className='glow-on-hover'>More</button>
+    </div>
+  )
+}
+
+export default MoreButton
